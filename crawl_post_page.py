4af13219cd9cd1faf06456c4a489c61090a55c43
hungnguyen10121311 <hungnguyen610a2@gmail.com>
2025-12-22 17:05:49 +0700

    2222

--- crawl_post_page.py
+++ crawl_post_page.py
@@ -17,7 +17,7 @@ def main():
     chrome_options = Options()
     chrome_options.add_argument("--incognito")
     chrome_options.add_argument("--disable-notifications")
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920x1080")
     driver = webdriver.Chrome(service=service, options=chrome_options)
